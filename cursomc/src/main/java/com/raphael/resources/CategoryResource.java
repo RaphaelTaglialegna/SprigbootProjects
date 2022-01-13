ad2d16386d6c51705d8068b43098dfc685447640
Raphael Taglialegna <raphae.trcosta@gmail.com>
2022-01-13 19:51:11 -0300

    Criando o Repository and Service

--- cursomc/src/main/java/com/raphael/resources/CategoryResource.java
+++ cursomc/src/main/java/com/raphael/resources/CategoryResource.java
@@ -1,31 +1,27 @@
 package com.raphael.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.raphael.domain.Category;
+import com.raphael.services.CategoryService;
 
 @RestController
 @RequestMapping(value="/category")
 public class CategoryResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Category> listar() {
+	@Autowired
+	private CategoryService service;
+	
+	@RequestMapping( value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Category obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 		
-		Category cat1 = new Category(1, "Informática");
-		Category cat2 = new Category(2, "Escritório");
-		
-		List<Category> lista = new ArrayList<>(); 
-		
-		lista.add(cat1);
-		lista.add(cat2);
-
-
-		return lista;
 	}
 	
 }
